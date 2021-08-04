15d5141b3efa81afe24d56d092ddfda63482733b
Mazed Mohammed <mazedislam21@gmail.com>
2021-08-04 11:09:17 +0600

    massage

--- src/components/HostCottage/Host.js
+++ src/components/HostCottage/Host.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import fakeData from '../Shared/FakeData/FakeData.json';
 
 const Host = () => {
